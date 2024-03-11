46c6eed5d649f2a4e2c610f43c420c23ca55bbd3
aladinor <aladinor@unal.edu.co>
2024-03-11 14:39:33 -0500

    redoing function including parameters

--- sigmet2zarr/task2zarr.py
+++ sigmet2zarr/task2zarr.py
@@ -33,82 +33,109 @@ def raw_to_dt(file: str) -> DataTree:
     return DataTree.from_dict({swps[k]: data[k] for k in list(data.keys())})
 
 
-def dt2zarr2(dt: DataTree, **kwargs: dict) -> None:
+def dt2zarr2(
+    dt: DataTree,
+    zarr_store: str,
+    zarr_version: int,
+    append_dim: str,
+    mode: str,
+    consolidated: bool,
+) -> None:
     """
     Functions to save xradar datatree using zarr format
+    @param consolidated: Xarray consolidated metadata. Default True
+    @param append_dim: dimension name where data will be appended. e.g. 'vcp_time'
+    @param mode: Xarray.to_zarr mode. Options are "w", "w-", "a", "a-", r+", None
+    @param zarr_version: data can be store in zarr format using version 2 or 3. Default V=2
+    @param zarr_store: path to zarr store
     @param dt: xradar datatree
-    @param kwargs: keyword arguments
-            - append_dim = dimension name where data will be appended. e.g. 'vcp_time'
-            - mode = Xarray.to_zarr mode. Options {"w", "w-", "a", "a-", r+", None}
     @return: None
     """
-    if kwargs["zarr_version"] == 3:
-        st = zarr.DirectoryStoreV3(kwargs["store"])
-    else:
-        st = zarr.DirectoryStore(kwargs["store"])
+    st = (
+        zarr.DirectoryStoreV3(zarr_store)
+        if zarr_version == 3
+        else zarr.DirectoryStore(zarr_store)
+    )
     nodes = st.listdir()
-    args = kwargs.copy()
     for child in list(dt.children):
         ds = dt[child].to_dataset()
         _time = convert_time(ds)
-        ds[kwargs["append_dim"]] = _time
-        ds = ds.expand_dims(dim=kwargs["append_dim"], axis=0).set_coords(
-            kwargs["append_dim"]
-        )
+        ds[append_dim] = _time
+        ds = ds.expand_dims(dim=append_dim, axis=0).set_coords(append_dim)
         if child in nodes:
-            ds.to_zarr(group=child, **args)
+            ds.to_zarr(
+                group=child,
+                mode=mode,
+                store=zarr_store,
+                zarr_version=zarr_version,
+                consolidated=consolidated,
+            )
         else:
-            try:
-                args = kwargs.copy()
-                del args["append_dim"]
-                args["mode"] = "w-"
-                encoding = {
-                    kwargs["append_dim"]: {
-                        "units": "milliseconds since 2000-01-01T00:00:04.010000",
-                        "dtype": "float",
-                    },
-                    "time": {
-                        "units": "milliseconds since 2000-01-01T00:00:04.010000",
-                        "dtype": "float",
-                    },
-                }
-                ds.to_zarr(group=child, encoding=encoding, **args)
-            except zarr.errors.ContainsGroupError:
-                args = kwargs.copy()
-                ds.to_zarr(group=child, **args)
+            mode = "w-"
+            encoding = {
+                append_dim: {
+                    "units": "milliseconds since 2000-01-01T00:00:04.010000",
+                    "dtype": "float",
+                },
+                "time": {
+                    "units": "milliseconds since 2000-01-01T00:00:04.010000",
+                    "dtype": "float",
+                },
+            }
+            ds.to_zarr(
+                group=child,
+                mode=mode,
+                store=zarr_store,
+                zarr_version=zarr_version,
+                consolidated=consolidated,
+                encoding=encoding,
+            )
 
 
-def raw2zarr(file, **kwargs) -> None:
+def raw2zarr(
+    file: str,
+    zarr_store: str,
+    zarr_version: int = 2,
+    elevation: list[float] = None,
+    append_dim: str = "vcp_time",
+    mode: str = "a",
+    consolidated: bool = True,
+    p2c: str = "../results",
+) -> None:
     """
-    Main function to convert sigmet radar files into xradar datatree
+    Main function to convert sigmet radar files into xradar datatree and save them using zarr format
+    @param consolidated: Xarray consolidated metadata. Default True
+    @param p2c: path to write a file where each radar filename will be written once is processed.
+    @param mode:  Xarray.to_zarr mode. Options are "w", "w-", "a", "a-", r+", None
+    @param append_dim: dimension name where data will be appended. e.g. 'vcp_time'
+    @param elevation: list of elevation to be converted into zarr.
+                      E.g. [0.5, 1.0, 3]. If None all sweeps within the radar object will be considered
+    @param zarr_version: data can be store in zarr format using version 2 or 3. Default V=2
+    @param zarr_store: path to zarr store
     @param file: radar file path
-    @param kwargs: keyword arguments passed to raw_to_dt
-                - elevation: sweep elevation to be considered.e.g [0.5, 1.0, 3]. If None all sweeps will be considered
     @return: None
     """
     dt = raw_to_dt(file)
     elevations = [
         np.round(np.median(dt.children[i].elevation.data), 1) for i in list(dt.children)
     ]
-    try:
-        if kwargs["elevation"] in elevations:
-            path2check = (
-                kwargs.get("path2check")
-                if kwargs.get("path2check") is not None
-                else "../results"
-            )
-            kwargs.setdefault("path2check", "../results")
-            del kwargs["elevation"], kwargs["path2check"]
-            dt2zarr2(dt=dt, **kwargs)
-            del dt
-            write_file_radar(file, path2check)
-    except KeyError:
-        path2check = (
-            kwargs.get("path2check")
-            if kwargs.get("path2check") is not None
-            else "../results"
+    if not elevation:
+        dt2zarr2(
+            dt=dt,
+            zarr_store=zarr_store,
+            zarr_version=zarr_version,
+            mode=mode,
+            consolidated=consolidated,
+            append_dim=append_dim,
         )
-        kwargs.setdefault("path2check", "../results")
-        del kwargs["path2check"]
-        dt2zarr2(dt=dt, **kwargs)
-        write_file_radar(file, path2check)
+        write_file_radar(file, p2c)
+    elif elevation in elevations:
+        dt2zarr2(
+            dt=dt,
+            zarr_store=zarr_store,
+            zarr_version=zarr_version,
+            mode=mode,
+            consolidated=consolidated,
+            append_dim=append_dim,
+        )
+        write_file_radar(file, p2c)

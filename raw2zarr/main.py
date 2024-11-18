4650e7cddf92110d6fde52650846f7b905371a72
aladinor <aladinor@unal.edu.co>
2024-11-18 13:42:24 -0600

    updating main script

--- raw2zarr/main.py
+++ raw2zarr/main.py
@@ -1,61 +1,54 @@
-from datetime import datetime
-from errno import EXDEV
-
-from raw2zarr.utils import (
-    create_query,
-    timer_func,
-    data_accessor,
-    time_encoding,
-)
-from raw2zarr.task2zarr import prepare2append
-from raw2zarr.dtree_builder import datatree_builder
 import fsspec
-import xradar as xd
+from datetime import datetime
+
+from raw2zarr.dtree_builder import append_sequential, append_parallel
+from raw2zarr.utils import timer_func, create_query
 
 
-def accessor_wrapper(filename):
-    return prepare2append(
-        xd.io.open_iris_datatree(data_accessor(filename)),
-        append_dim="vcp_time",
-        radar_name="GUA",
-    )
+def get_radar_files(engine):
+
+    fs = fsspec.filesystem("s3", anon=True)
+
+    if engine == "iris":
+        radar_name = "Guaviare"
+        year, month, day = 2022, 6, 1
+        date_query = datetime(year=year, month=month, day=day)
+        str_bucket = "s3://s3-radaresideam/"
+        query = create_query(date=date_query, radar_site=radar_name)
+        radar_files = [f"s3://{i}" for i in sorted(fs.glob(f"{str_bucket}{query}*"))][
+            550:570
+        ]
+        zs = f"../zarr/{radar_name}.zarr"
+        return radar_files, zs, "iris"
+    elif engine == "nexradlevel2":
+        # NEXRAD
+        zs = "../zarr/nexrad2.zarr"
+        query = f"2023/06/29/KILX/KILX"
+        str_bucket = "s3://noaa-nexrad-level2/"
+        radar_files = [f"s3://{i}" for i in sorted(fs.glob(f"{str_bucket}{query}*"))]
+        radar_files = radar_files[110:200]
+        return radar_files, zs, "nexradlevel2"
 
 
 @timer_func
-def radar_convert():
-    radar_name = "Guaviare"
-    year, month, day = 2022, 6, 1  # Guaviare
-
-    date_query = datetime(year=year, month=month, day=day)
-    query = create_query(date=date_query, radar_site=radar_name)
-    str_bucket = "s3://s3-radaresideam/"
-    fs = fsspec.filesystem("s3", anon=True)
-    radar_files = [f"s3://{i}" for i in sorted(fs.glob(f"{str_bucket}{query}*"))][0:12]
-
-    dtree = datatree_builder(radar_files, batch_size=4)
-    time_enc = time_encoding(dtree, append_dim="vcp_time")
-    zv = 2
-    try:
-        path = "../zarr/Guaviare_v2.zarr" if zv == 2 else "../zarr/Guaviare_v3.zarr"
-        _ = dtree.to_zarr(
-            path,
-            encoding=time_enc,
-            consolidated=True,
-            mode="a-",
-            zarr_format=zv,
-        )
-    except ValueError:
-        _ = dtree.to_zarr(
-            "../zarr/Guaviare_new.zarr",
-            consolidated=True,
-            mode="a-",
-            zarr_format=zv,
-        )
-    print(dtree)
-
-
 def main():
-    radar_convert()
+    # IRIS Colombia
+    radar_files, zs, engine = get_radar_files("iris")
+
+    # NEXRAD
+    # radar_files, zs, engine  = get_radar_files('nexradlevel2')
+
+    zarr_format = 2
+    append_dim = "vcp_time"
+    # append_sequential(radar_files, zarr_store=zs, append_dim=append_dim, zarr_format=zarr_format)
+    append_parallel(
+        radar_files,
+        zarr_store=zs,
+        append_dim=append_dim,
+        zarr_format=zarr_format,
+        batch_size=10,
+        engine=engine,
+    )
 
 
 if __name__ == "__main__":

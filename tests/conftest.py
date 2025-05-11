7a5bab07873b4d737dbe88543c33b57ccf15a812
aladinor <aladinor@unal.edu.co>
2025-05-11 16:31:07 -0500

    adding nexrad_Aws_file_sweep10_misaligned

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,21 @@ import pytest
 
 
 @pytest.fixture(scope="session")
-def nexrad_local_str_file(tmp_path_factory):
+def nexrad_aws_file_sweep10_misaligned(tmp_path_factory):
+    """
+    Download a real NEXRAD file with known azimuth and angle misalignment (sweep_10) from AWS.
+    Used for fix_angle/fix_azimuth tests.
+    """
+    aws_url = "s3://noaa-nexrad-level2/2023/06/29/KILX/KILX20230629_154426_V06"
+    local_path = tmp_path_factory.mktemp("nexrad_data") / "KILX20230629_154426_V06"
+    with fsspec.open(aws_url, anon=True) as s3_file:
+        with open(local_path, "wb") as local_file:
+            local_file.write(s3_file.read())
+    return str(local_path)
+
+
+@pytest.fixture(scope="session")
+def nexrad_local_uncompressed_file(tmp_path_factory):
     """
     Download a NEXRAD radar file from AWS S3 to a temporary directory for testing.
     """
@@ -25,7 +39,7 @@ def nexrad_local_str_file(tmp_path_factory):
 
 
 @pytest.fixture(scope="session")
-def nexrad_gz_local_file(tmp_path_factory):
+def nexrad_local_gz_file(tmp_path_factory):
     """
     Download a GZIP NEXRAD file from AWS S3 for testing.
     """

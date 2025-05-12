30ca92d701ea10236a3e3066bad1d0baabd8dacc
aladinor <aladinor@unal.edu.co>
2025-05-12 11:58:06 -0500

    fixing numpy version

--- tests/transform/test_encoding.py
+++ tests/transform/test_encoding.py
@@ -1,4 +1,5 @@
 import numpy as np
+from packaging.version import parse as parse_version
 
 from raw2zarr.transform.encoding import dtree_encoding
 
@@ -14,13 +15,18 @@ def test_dtree_encoding_full_structure(radar_dtree_factory):
     assert root_enc[append_dim]["units"].startswith("nanoseconds")
     assert root_enc[append_dim]["_FillValue"] == -9999
 
+    if parse_version(np.__version__) >= parse_version("2.0.0"):
+        str_encoding = np.dtypes.StringDType
+    else:
+        str_encoding = np.dtype("U")
+
     for field in [
         "platform_type",
         "instrument_type",
         "time_coverage_start",
         "time_coverage_end",
     ]:
-        assert root_enc[field]["dtype"] == np.dtypes.StringDType
+        assert root_enc[field]["dtype"] == str_encoding
 
     assert "/sweep_0" in encoding
     sweep_enc = encoding["/sweep_0"]
@@ -34,7 +40,7 @@ def test_dtree_encoding_full_structure(radar_dtree_factory):
         assert sweep_enc[field]["_FillValue"] == -9999
 
     for field in ["sweep_mode", "prt_mode", "follow_mode"]:
-        assert sweep_enc[field]["dtype"] == np.dtypes.StringDType
+        assert sweep_enc[field]["dtype"] == str_encoding
 
     for field in ["sweep_number", "sweep_fixed_angle"]:
         assert sweep_enc[field]["_FillValue"] == -9999

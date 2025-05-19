92580b6f4a8e71ca866a84dc8b5fb6be94ae419b
aladinor <aladinor@unal.edu.co>
2025-05-19 17:35:43 -0500

    updating init file

--- raw2zarr/__init__.py
+++ raw2zarr/__init__.py
@@ -1,36 +1,30 @@
 """
 raw2zarr
-======
+========
 
-Top-level package for raw2zarr.
+High-level API for converting raw radar data to Zarr.
 
+Author: Alfonso Ladino
+Email: alfonso8@illinois.edu
+Version: 2025.1.0
 """
 
-__author__ = """Alfonso Ladino"""
+__author__ = "Alfonso Ladino"
 __email__ = "alfonso8@illinois.edu"
-__version__ = "2025.1.0."
+__version__ = "2025.1.0"
 
-from .dtree_builder import (
+from raw2zarr.builder.dtree_builder import (
     append_parallel,
     append_sequential,
     datatree_builder,
     process_file,
 )
-from .dtree_io import _load_file, load_radar_data, prepare2read
-from .utils import batch, dtree_encoding, ensure_dimension, fix_angle
-from .zarr_writer import dtree2zarr
+from raw2zarr.writer.zarr_writer import dtree2zarr
 
 __all__ = [
     "datatree_builder",
+    "process_file",
     "append_sequential",
     "append_parallel",
-    "load_radar_data",
-    "ensure_dimension",
-    "fix_angle",
-    "batch",
-    "dtree_encoding",
-    "process_file",
-    "prepare2read",
-    "_load_file",
     "dtree2zarr",
 ]

e6bc9c5b46ddd8736ef94c497d3f22bf38d5971f
aladinor <aladinor@unal.edu.co>
2025-07-23 16:45:04 -0500

    adding doc to nexrad loader

--- raw2zarr/io/loaders/nexrad.py
+++ raw2zarr/io/loaders/nexrad.py
@@ -7,9 +7,6 @@ from ..preprocess import normalize_input_for_xradar
 def nexradlevel2_loader(file) -> DataTree:
     """
     Loads NEXRAD Level 2 files from local or S3, decompresses if needed.
-    Returns a local uncompressed path to xradar, which does not support streaming.
     """
-    # TODO: use prepare2read when xradar supports streaming for nexradlevel2 engine.
-    #  See https://github.com/openradar/xradar/issues/265
     path = normalize_input_for_xradar(file)
     return open_nexradlevel2_datatree(path)
